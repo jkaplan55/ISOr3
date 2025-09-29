303b85863e0ef459b8946d0a31a836e7f2c4bc02
Joe <jkaplan55@gmail.com>
2025-09-28 17:21:19 -0700

    Updated minimum cmake version to 3.5 since 3.0 is no longer supported.

--- source/projects/ISOr3.signin/ISOr3.signin.cpp
+++ source/projects/ISOr3.signin/ISOr3.signin.cpp
@@ -1,4 +1,4 @@
-﻿/// @file
+/// @file
 ///	@ingroup 	minexamples
 ///	@copyright	Copyright 2018 The Min-DevKit Authors. All rights reserved.
 ///	@license	Use of this source code is governed by the MIT License found in the License.md file.
